26cfd70b6a26e7bcaeea1d49fd5f88da56d70541
juanmaramirezgarcia <101336518+juanmaramirezgarcia@users.noreply.github.com>
2022-05-06 20:28:00 +0200

    Update train_model.sh
    "TRAINING_DATA_ASSET_ID=$training_data_asset_id"

--- samples/jenkins/scripts/dev/train_model.sh
+++ samples/jenkins/scripts/dev/train_model.sh
@@ -50,7 +50,7 @@ cat > job.json <<-EOJSON
             "CPD_URL=$cpd_url",
             "USER_NAME=$user_name",
             "USER_PASSWORD=$user_apikey",
-            "DATA_ASSET_ID=$training_data_asset_id",
+            "TRAINING_DATA_ASSET_ID=$training_data_asset_id",
             "MODEL_NAME=$model_name"
         ]
     }
