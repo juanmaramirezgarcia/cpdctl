61af9537085d15292c42e9f236fa9de25586c371
juanmaramirezgarcia <101336518+juanmaramirezgarcia@users.noreply.github.com>
2022-05-07 13:46:55 +0200

    Update train_model.sh
    updated json env variables

--- samples/jenkins/scripts/dev/train_model.sh
+++ samples/jenkins/scripts/dev/train_model.sh
@@ -47,11 +47,18 @@ cat > job.json <<-EOJSON
         "env_id": "$env_id",
         "env_type": "notebook",
         "env_variables": [
-            "CPD_URL"="$cpd_url",
-            "USER_NAME"="$user_name",
-            "USER_PASSWORD"="$user_apikey",
-            "TRAINING_DATA_ASSET_ID"="$training_data_asset_id",
-            "MODEL_NAME"="$model_name"
+            {
+#            "CPD_URL"="$cpd_url",
+#            "USER_NAME"="$user_name",
+#            "USER_PASSWORD"="$user_apikey",
+             "name": {
+                "name": "TRAINING_DATA_ASSET_ID"
+                },
+              "value": {
+                 "value": "$training_data_asset_id"
+                 }
+#            "MODEL_NAME"="$model_name"
+           }
         ]
     }
 }
